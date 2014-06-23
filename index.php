7fb5f4b9fbb81dd366af6c7f0c7b222601c12cc6
Kate Levin <klevin@us.westfield.com>
2014-06-22 23:24:57 -0700

    using node, because php stinks... moved css and images to public

--- index.php
+++
@@ -1,7 +0,0 @@
-<?
-$uri = explode('/', $_SERVER['REQUEST_URI']);
-$template = (empty($uri[1])) ? 'home' : $uri[1];
-
-require_once('header.html');
-require_once('template/'. $template . '.html');
-require_once('footer.html');
