51da8cac6265b981b7cf15b78b9da0410718c74c
Chris Pearce <cpearce@au.westfield.com>
2014-07-10 13:09:24 +1000

    First round of Reg 3 UI

--- public/js/script.js
+++ public/js/script.js
@@ -55,48 +55,94 @@ if (isiPhone) {
 }
 
 /**
- * Avoid `console` errors in browsers that lack a console.
+ * Define functions
  */
 
+// Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-      'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-      'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-      'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-      'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-    while (length--) {
-      method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-      // Only stub undefined methods.
-      if (!console[method]) {
-        console[method] = noop;
-      }
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
     }
+  }
 }());
 
+// Apply a style hook to each form field label when their inputs are focused
+var formLabelFocus = function() {
+  var formLabelFocusHighlight = '.js-form-label-focus';
+
+  $(formLabelFocusHighlight).on('focus blur', 'input, select', function() {
+    var input = $(this);
+    setTimeout(function() {
+      input.closest(formLabelFocusHighlight).toggleClass('is-focused', input.is(':focus'));
+    }, 0 );
+  });
+}
+
+// Highlight relevant credit card image determined by the first number a user types into the credit card number input
+var determineCardType = function() {
+
+  var determineCardTypeElem = $('.js-credit-card-type');
+  var determineCardTypeAmex = $('.js-credit-card-type-amex');
+  var determineCardTypeVisa = $('.js-credit-card-type-visa');
+  var determineCardTypeMc = $('.js-credit-card-type-mc');
+
+  var determineCardTypeEvent = function() {
+    var firstNum = $(this).val().slice(0,1);
+    determineCardTypeElem.removeClass('is-visible').removeClass('is-invisible');
+    switch(firstNum) {
+      case '3':
+        determineCardTypeAmex.addClass('is-visible');
+        determineCardTypeVisa.addClass('is-invisible');
+        determineCardTypeMc.addClass('is-invisible');
+        break;
+      case '4':
+        determineCardTypeVisa.addClass('is-visible');
+        determineCardTypeAmex.addClass('is-invisible');
+        determineCardTypeMc.addClass('is-invisible');
+        break;
+      case '5':
+        determineCardTypeMc.addClass('is-visible');
+        determineCardTypeAmex.addClass('is-invisible');
+        determineCardTypeVisa.addClass('is-invisible');
+        break;
+    }
+  }
+
+  $(document).on('keyup', '.js-credit-card-type-input', determineCardTypeEvent);
+}
+
+// Payment type selection for Registration 3
+var paymentTypeSelection = function() {
+
+  var hideByDefault = $('.js-payment-type-hide-default');
+
+  hideByDefault.hide();
+
+}
 
 /**
- * Do stuff on DOM ready
+ * Initialise functions
  */
 
 $(function() {
 
-	/**
-	 * Apply a style hook to each form field label when their inputs are focused
-	 */
-  var formLabelFocusHighlight = '.js-form-label-focus';
-
-	$(formLabelFocusHighlight).on('focus blur', 'input, select', function() {
-	  var input = $(this);
-	  setTimeout(function() {
-	  	input.closest(formLabelFocusHighlight).toggleClass('is-focused', input.is(':focus'));
-	  }, 0 );
-	});
+  formLabelFocus();
+  determineCardType();
+  paymentTypeSelection();
 
 });
