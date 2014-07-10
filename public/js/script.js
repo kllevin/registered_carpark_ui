f2ef78f762774a3e60cb5613c6e264f28fdbf77f
Kate Levin <klevin@us.westfield.com>
2014-07-10 16:09:41 +1000

    add cursor style to faux-labels, add js hooks to focus on first input or selector after click on a faux label

--- public/js/script.js
+++ public/js/script.js
@@ -93,6 +93,16 @@ var formLabelFocus = function() {
   });
 }
 
+//Faux label
+var fauxLabelFocus = function() {
+  var fauxLabel = '.js-faux-label';
+  var fauxLabelFocus = '.js-faux-label-focus';
+
+  $(fauxLabel).on('click', function() {
+    $(fauxLabelFocus).find('select, input').eq(0).focus();
+  });
+}
+
 // Highlight relevant credit card image determined by the first number a user types into the credit card number input
 var determineCardType = function() {
 
@@ -142,6 +152,7 @@ var paymentTypeSelection = function() {
 $(function() {
 
   formLabelFocus();
+  fauxLabelFocus();
   determineCardType();
   paymentTypeSelection();
 
