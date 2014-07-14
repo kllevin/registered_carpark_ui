19cd55328cd976c4b8936092f8fbfc5aa734695e
Chris Pearce <cpearce@au.westfield.com>
2014-07-14 12:25:10 +1000

    Removing CCV from Registration 3 screen

--- public/js/script.js
+++ public/js/script.js
@@ -148,7 +148,8 @@ var paymentTypeSelection = function() {
   var inputPayStation = $('.js-payment-type-paystation-input');
 
   // Hide/show elements based on the above radio inputs
-  var hidePayPerVisit = $('.js-payment-type-pay-per-visit-show');
+  var hidePayPerVisit = $('.js-payment-type-pay-per-visit-hide');
+  var showPayPerVisit = $('.js-payment-type-pay-per-visit-show');
   var hidePrepaid = $('.js-payment-type-prepaid-hide');
   var hidePayStation = $('.js-payment-type-paystation-hide');
   var showPayStation = $('.js-payment-type-paystation-show');
@@ -164,14 +165,16 @@ var paymentTypeSelection = function() {
     showPayStation.hide();
     hidePayStation.show();
     hidePrepaid.show();
-    hidePayPerVisit.hide();
+    hidePayPerVisit.show();
+    showPayPerVisit.hide();
     // Make the grid item half width to balance the grid rows
     miscPaymentTypeGridItem.removeClass('one-whole').addClass('non-palm-one-half');
   });
 
   // Pay per visit radio
   inputPayPerVisit.change(function() {
-    hidePayPerVisit.show();
+    hidePayPerVisit.hide();
+    showPayPerVisit.show();
     hidePrepaid.hide();
     hidePayStation.show();
     showPayStation.hide();
